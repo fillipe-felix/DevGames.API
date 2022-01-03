9903e3a132afddceae57c6fb9af8bee6c8eeab26
fillipe.felix <felipesoares_1993@hotmail.com>
2022-01-03 11:24:44 -0300

    Adicionando a migration para ter User em Post

---
+++ DevGames.API/Persistence/Migrations/20220103015927_AddUserInEntitiePost.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevGames.API.Persistence.Migrations
+{
+    public partial class AddUserInEntitiePost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "User",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "User",
+                table: "Posts");
+        }
+    }
+}
